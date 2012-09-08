389e444162b82c90fb1b692c3099e2ba6bbd8262
Fabio Rehm <fgrehm@gmail.com>
2012-09-07 23:51:46 -0300

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+symlinks=( vimrc vim )
+
+echo "Creating symlinks..."
+for link in ${symlinks[@]}; do
+  target="`pwd`/${link}"
+  link_name="${HOME}/.${link}"
+  if ! [[ -L $link_name ]]; then
+    echo "  LINK - ${link_name}"
+    ln -s $target $link_name
+  else
+    echo "  SKIP - ${link_name}"
+  fi
+done
+
+if ! [[ -d `pwd`/vim/bundle/vundle/.git  ]]; then
+  echo "Cloning Vundle"
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+else
+  echo "Skipping Vundle clone"
+fi
+
+echo -e "\nOpen up vim and run :BundleInstall"
